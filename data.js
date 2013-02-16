1be1000148ac2fb44ea29a7174b48e6c6e1199fe
Joe Hitchens <joe@sleepless.com>
2013-02-15 23:32:57 -0800

    -

---
+++ data.js
@@ -0,0 +1,39 @@
+
+var data = {
+
+	menudata: [
+		{ page: "./page1.html", label: "Page #1", },
+		{ page: "./page2.html", label: "Page #2", },
+	],
+
+	first: [
+		{ heading: "General", name:"Washington", when:"Jul 4, 1776" },
+		{ heading: "Sailer", name:"Columbus", when: "" },
+		{ heading: "Hottie", name:"Nightingale", when: "" },
+	],
+	second: [
+		{heading: "Earth", name:"Home of the brave", when:""},
+		{heading: "Mars", name:"Home of the green", when:""},
+		{heading: "Venus", name:"Home of the hotties", when:""},
+	],
+
+	subdata: [
+		{ section: "Section A",
+			list_tmpl:[
+				{ name: "Alice", desc: "Lonely narcotics officer." }, 
+				{ name: "Annie", desc: "Distressed single mom." }, 
+				{ name: "Ashton", desc: "Psycopath with nice wardrobe." }, 
+			], },
+		{ section: "Section B",
+			list_tmpl:[
+				{ name: "Bob", desc: "An old hippy." }, 
+			], },
+		{ section: "Section C",
+			list_tmpl:[
+				{ name: "Curt", desc: "Bad actor." }, 
+				{ name: "Candy", desc: "Sweet treat." }, 
+			], },
+	],
+
+};
+
